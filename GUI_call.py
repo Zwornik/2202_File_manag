72eeabcd19b1e9e3c242c133b507ffa846932b7a
Zwornik <kruszelnickim@gmail.com>
2022-08-26 23:34:38 +0100

    Layout revised, progress with checkBoxes

--- GUI_call.py
+++ GUI_call.py
@@ -8,14 +8,7 @@ import logging
 from datetime import datetime as dt
 # from File_manag import *
 
-
-# file_list = [('20160130_215245.jpg', '.jpg', '2 724', '2021.09.23 20:11:22', 'D:\\TEMP\\20160130_215245.jpg'), ('20160130_215331sadasdasdasdasdasdd.jpg', '.jpg', '2 689', '2022.08.18 16:26:14', 'D:\\TEMP\\20160130_215331sadasdasdasdasdasdd.jpg'), ('DSC_0044.JPG', '.JPG', '3 440', '2022.05.28 17:59:00', 'D:\\TEMP\\DSC_0044.JPG'), ('DSC_0044.NEF', '.NEF', '15 991', '2022.05.28 17:59:00', 'D:\\TEMP\\DSC_0044.NEF'), ('DSC_0045.JPG', '.JPG', '4 055', '2022.05.28 17:59:14', 'D:\\TEMP\\DSC_0045.JPG')]
-item = ('20160130_215245.jpg', '.jpg', '2 724', '2021.09.23 20:11:22', 'D:\\TEMP\\20160130_215245.jpg')
-Name = '20160130_215245.jpg'
-Size = '2 724'
-Path = 'D:\\TEMP\\20160130_215245.jpg'
-items = []
-c=0
+logging.basicConfig(level=logging.ERROR)
 
 class MyWindow(QMainWindow):
 
@@ -34,62 +27,118 @@ class MyWindow(QMainWindow):
 
 		# Define main window Widgets
 		self.label_A = self.findChild(QLabel, "label_A")
-		self.path_line_A = self.findChild(QLineEdit, "path_line_A")
+		self.label_B = self.findChild(QLabel, "label_B")
+
+		self.path_label_A = self.findChild(QLabel, "path_label_A")
+		self.path_label_B = self.findChild(QLabel, "path_label_B")
+
 		self.browse_A = self.findChild(QPushButton, "Browse_A")
 		self.browse_B = self.findChild(QPushButton, "Browse_B")
+		self.display_files_A = self.findChild(QPushButton, "display_files_A")
+		self.display_files_B = self.findChild(QPushButton, "display_files_B")
+		self.find_dup = self.findChild(QPushButton, "find_duplicates")
+		self.delete_A = self.findChild(QPushButton, "delete_A")
+		self.delete_B = self.findChild(QPushButton, "delete_B")
 
-		self.statusbBar = self.findChild(QStatusBar, "statusbar")
-		self.statusbBar.setFont(QFont("Arial", 12))
+		self.subf_check_A = self.findChild(QCheckBox, "subfolders_A")
+		self.subf_check_B = self.findChild(QCheckBox, "subfolders_B")
+		self.exif_check_A = self.findChild(QCheckBox, "find_EXIF_A")
+		self.exif_check_B = self.findChild(QCheckBox, "find_EXIF_B")
 
-		# Tree A
+		self.statusBar = self.findChild(QStatusBar, "statusbar")
+		self.statusBar.setFont(QFont("Arial", 18))
+
 		self.tree_A = self.findChild(QTreeWidget, "tree_A")
-		item = QTreeWidgetItem([Name])
-		Siz = QTreeWidgetItem([Size])
-		item.addChild(Siz)
-		self.tree_A.insertTopLevelItems(0, [item])
-
-		# Tree B
 		self.tree_B = self.findChild(QTreeWidget, "tree_B")
-		item = QTreeWidgetItem([Name])
-		Siz = QTreeWidgetItem([Size])
-		item.addChild(Siz)
-		self.tree_B.insertTopLevelItems(0, [item])
+
 
 		# Acctions
-		self.browse_A.clicked.connect(lambda x: self.browse("A")) # Button A
-		self.browse_B.clicked.connect(lambda x: self.browse("B")) # Button B
+		self.browse_A.clicked.connect(lambda x: self.browse("A"))  # Browse button A clicked
+		self.browse_B.clicked.connect(lambda x: self.browse("B"))  # Browse button B clicked
+		self.display_files_A.clicked.connect(lambda x: self.walk_folders("A") if self.clear_tree("A") == True else self.clear_tree("A") )
+		self.display_files_A.clicked.connect(lambda x: self.clear_tree("B"))
 
-		# self.clear.clicked.connect(self.clearer)
+		# self.find_dup.clicked.connect(lambda x: self.browse("A"))  # Find duplicates button clicked
+		# self.delete_A.clicked.connect(lambda x: self.browse("B"))  # Delete from location A button clicked
+		# self.delete_B.clicked.connect(lambda x: self.browse("B"))  # Delete from location B button clicked
+
+		self.path_label_A.setText("C:/TEMP")
+		self.path_label_B.setText("C:/TEMP")
 
 		# Showing the App
 		self.show()
 
 
-
 	def browse(self, side):
 		"""Opens dialog selecting folder location"""
 
+		self.side = side
 		dialog = QFileDialog(self)
-		dialog.setDirectory("C:/")
+		dialog.setDirectory(self.path_label_A.text()) if side == "A" else dialog.setDirectory(self.path_label_B.text())
 		path = dialog.getExistingDirectory()
-		print(path)
-		self.walk_folders(path)
+		self.displ_path(path)
 
 
-	def show_files(self, file_list):
-		"""Display files in corresponding tree"""
-		print("aaaaaaaaaaaaaa", file_list)
+	def displ_path(self, path):
+		"""Display selected path in corresponding label"""
+
+		self.path_label_A.setText(path) if self.side == "A" else self.path_label_B.setText(path)  # Display to tree A or B
+
+
+	def clear_tree(self, side):
+
+
+		self.side = side
+		if self.side == "A":
+			self.tree_A.clear()
+		else:
+			self.tree_B.clear()
+
+		items = [QTreeWidgetItem(["Wait...................."])]
+		self.tree_A.insertTopLevelItems(0, items)
+		return True
+
+
+	def walk_folders(self, side):
+		"""WALKING THROUGH ALL FILES IN FOLDER AND SUB FOLDERS"""
+
 		items = []
-		# side = 0
-		# if side == "A":
-		# 	self.tree_A.clear()
-		# 	self.tree_A.insertTopLevelItems(items)
-		# else:
-		self.tree_B.clear()
-		for i in file_list:
-			items.append(QTreeWidgetItem(i))
+		self.side = side
+
+		# self.clear_tree()
+
+		if side == "A":   # reads path from label
+			path = self.path_label_A.text()
+		else:
+			path = self.path_label_B.text()
+
+		for path, subdirs, files in os.walk(path):
+			for item in os.scandir(path):
+				if item.is_file():
+					path = item.path
+					date = self.oldest_date(path)
+					name = item.name
+					file_type = os.path.splitext(item)[1]
+					file_date = self.dateformat(self.ts_to_dt(item.stat().st_atime))
+					size = ("{:,.0f} KB".format(item.stat().st_size / 1000).replace(",", " "))
+
+					items.append(QTreeWidgetItem([name, file_type, size, date, path]))
 		print(items)
-		self.tree_B.insertTopLevelItems(0, items)
+		self.show_files(items)
+
+
+	def show_files(self, items):
+		"""Display files in corresponding tree and count in label"""
+
+		if self.side == "A":  # Display to tree A or B
+			self.tree_A.clear()
+			self.tree_A.insertTopLevelItems(0, items)
+			self.label_A.setText("{} files found".format(self.tree_A.topLevelItemCount()))
+		else:
+			self.tree_B.clear()
+			self.tree_B.insertTopLevelItems(0, items)
+			self.label_B.setText("{} files found".format(self.tree_B.topLevelItemCount()))
+
 
 	def ts_to_dt(self, ts):
 		return dt.fromtimestamp(ts)
@@ -98,53 +147,31 @@ class MyWindow(QMainWindow):
 		date_string = date_string.strftime("%Y.%m.%d %H:%M:%S")
 		return date_string
 
-	def oldest_date(self, path, date_extract):
-		"""EXTRACTING 3 PICTURE CREATION DATES AND RETURNS THE OLDEST ONE"""
+	def oldest_date(self, path):
+		"""EXTRACT 3 PICTURE CREATION DATES AND RETURNS THE OLDEST ONE"""
+		print("AAAA" , self.exif_check_A.checkState())
 
 		date_m = self.dateformat(self.ts_to_dt(os.path.getmtime(path)))  # Modification date
 		date_c = self.dateformat(self.ts_to_dt(os.path.getctime(path)))  # File creation date
 
-		if date_extract == "Y":
+		if self.side == "A" and self.exif_check_A.checkState() or self.side == "B" and self.exif_check_B.checkState():
 
-			# reading EXIF date from picture reader
+			# reading EXIF date
 			try:
 				file = open(path, 'rb')  # opens file to check if EXIF tag is there with date of picture taken
 				tags = exifread.process_file(file, stop_tag="EXIF DateTimeOriginal")
-				date_t = str(tags["EXIF DateTimeOriginal"])  # Picture taken date
+				date_exif = str(tags["EXIF DateTimeOriginal"])  # Picture taken date
 			except:
-				date_t = "Z"
+				date_exif = "Z"
 				pass
 			else:
-				date_t = date_t[:4] + "." + date_t[5:7] + "." + date_t[8:]  # replacing : with . in date format
-			date = sorted([date_t, date_c, date_m])[0]  # Selecting the oldest date
+				date_exif = date_exif[:4] + "." + date_exif[5:7] + "." + date_exif[8:] + "EXIF"  # replacing ':' with '.' in date format
+			date = sorted([date_exif, date_c, date_m])[0]  # Selecting the oldest date
 
 		else:
 			date = sorted([date_c, date_m])[0]
+		self.statusBar.showMessage("checked?  {}".format(date))
 		return date
-
-	def walk_folders(self, path):
-		"""WALKING THROUGH ALL FILES IN FOLDER AND SUB FOLDERS"""
-		print(path)
-		file_list = []
-		for path, subdirs, files in os.walk(path):
-			for item in os.scandir(path):
-				if item.is_file():
-					# self.c += 1
-					path = item.path
-					date = self.dateformat(self.ts_to_dt(os.path.getctime(path)))
-					name = item.name
-					file_type = os.path.splitext(item)[1]
-					file_date = self.dateformat(self.ts_to_dt(item.stat().st_atime))
-					size = ("{:,.0f}".format(item.stat().st_size / 1000).replace(",", " "))
-
-					file_list.append([name, file_type, size, date, path])
-
-
-			# print("{:<30s}   {}  {:>12} KB   {}   {}".format(name, file_type, size, date, path))
-
-			# print(type(name), type(file_type), type(size), type(file_date), type(date), type(path))
-		print(file_list)
-		self.show_files(file_list)
 
 
 
@@ -154,19 +181,8 @@ class MyWindow(QMainWindow):
 		print(self.path_line_A.text())
 		self.path_line_A.clear()
 		self.path_line_A.setText("Dupa")
-		self.statusbBar.showMessage("checked?  {}".format(checked))
+
 		self.path_line_A.setClearButtonEnabled(True)
-
-		# self.label_A.setText("Hello {}".format(self.path_line_A.toPlainText()))
-		# self.label_A.adjustSize()
-		# self.path_line_A.setPlainText("")
-
-	# def clearer(self):
-	# 	self.label.setText("Again, enter your name:")
-	# 	self.textedit.setPlainText("")
-
-	# def add(self, item):
-
 
 
 
@@ -174,3 +190,4 @@ class MyWindow(QMainWindow):
 app = QApplication(sys.argv)
 UIWindow = MyWindow()
 app.exec_()
+print("DUpa")
